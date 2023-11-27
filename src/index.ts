fd5973dd94a118db1ebec0af12c15f28bd5dfb38
João Victor <jvbatistagomes11@gmail.com>
2023-11-27 13:37:36 -0300

    feat: add express inital server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log("Hello Word!");
+const express = require("express");
+import { config } from "dotenv";
+config();
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Aberto em ${PORT}`);
+});
+
+app.get("/", (req: any, res: any) => {
+  res.send("Hello Word");
+});
